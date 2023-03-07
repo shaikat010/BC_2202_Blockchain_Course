21dde41329710a65e89249ea16e97626d54fade3
Shaikat Majumder <68814937+shaikat010@users.noreply.github.com>
2023-03-07 15:23:08 +0600

    Add files via upload

---
+++ Peer_2_Peer_Chat/Peer_01.py
@@ -0,0 +1,32 @@
+from pubnub.callbacks import SubscribeCallback
+from pubnub.enums import PNStatusCategory
+from pubnub.pnconfiguration import PNConfiguration
+from pubnub.pubnub import PubNub
+import time
+import os
+pnconfig = PNConfiguration()
+userId = os.path.basename(__file__)
+pnconfig.publish_key = 'demo'
+pnconfig.subscribe_key = 'demo'
+pnconfig.user_id = userId
+pnconfig.ssl = True
+pubnub = PubNub(pnconfig)
+def my_publish_callback(envelope, status):
+  # Check whether request successfully completed or not
+  if not status.is_error():
+    pass
+class MySubscribeCallback(SubscribeCallback):
+  def presence(self, pubnub, presence):
+    pass
+  def status(self, pubnub, status):
+    pass
+  def message(self, pubnub, message):
+    if message.publisher == userId : return
+    print ("from device " + message.publisher + ": " + message.message)
+pubnub.add_listener(MySubscribeCallback())
+pubnub.subscribe().channels("chan-1").execute()
+# publish a message
+while True:
+  msg = input("")
+  if msg == 'exit': os._exit(1)
+  pubnub.publish().channel("chan-1").message(str(msg)).pn_async(my_publish_callback)
